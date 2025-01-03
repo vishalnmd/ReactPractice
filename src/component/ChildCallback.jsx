c0f2260f16abd012a58621ae8f0af80cc01baf42
Vishal <vishalkhairagarh@gmail.com>
2025-01-03 12:51:08 +0530

    implementing useCallback to resist unneccessary rending of child component

--- src/component/ChildCallback.jsx
+++ src/component/ChildCallback.jsx
@@ -5,7 +5,8 @@ const ChildCallback = React.memo((props)=>{
     return (
         <>
             <h3>this is child component</h3>
-            <p>when count is passed down to child component from parent component, It always renders the child component</p>
+            <button onClick={props.onClick}>Click me</button>
+            <p>when a property is passed down to child component from parent component, It always renders the child component</p>
         </>
     )
 });
--- src/component/UseCallback.jsx
+++ src/component/UseCallback.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import ChildCallback from "./ChildCallback";
 
 export default function UseCallback() {
 
     let [count, setCount] = useState(0);
 
+    const clickMe = useCallback(()=>{
+        console.log("button clicked");
+    },[]);
+
     return (
         <div className="flex flex-col justify-center items-center ">    
            
            <h2 className="p-2">Counter is : {count}</h2>
            <button onClick={()=>setCount(count+1)}>Increment</button>
-
-           <ChildCallback count={count}/>
+           <ChildCallback onClick={clickMe}/>
         </div>
     );
 }
