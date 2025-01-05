c7eaf4f9f53bcff4ed088551aa4aaa038067f53a
Vishal <vishalkhairagarh@gmail.com>
2025-01-05 12:48:55 +0530

    bug fix for card animation

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -3,7 +3,7 @@ function Card(props){
     return (
         <>
             <div className="h-64 w-56  gap-2 flex flex-col justify-center items-center
-                             bg-gray-500 text-sm hover:-translate-y-5 shadow-2xl rounded-lg">
+                             bg-gray-500 text-sm active:-translate-y-5 shadow-2xl rounded-lg md:hover:-translate-y-5 ">
                 <h2>{props.name}</h2>
                 <img className="size-24 pt-1 rounded-full" src={props.dir} alt="#" />
                 <p>{props.des}</p>
