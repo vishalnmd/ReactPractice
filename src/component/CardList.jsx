7a8f02a31298d6ca038ae45f2379a0357c510e1d
Vishal <vishalkhairagarh@gmail.com>
2025-01-05 11:40:28 +0530

    local image ref bug fix

--- src/component/CardList.jsx
+++ src/component/CardList.jsx
@@ -1,12 +1,15 @@
 import Card from "./Card";
+import EndouImg from "../assets/endou.jpg";
+import LuffyImg from "../assets/luffy.jpg";
+import ItachiImg from "../assets/itachi.jpeg";
 
 function CardList(){
     return (
         <>
             <div className="flex justify-center items-center sm:flex-row flex-col gap-6">
-                <Card name="Endou momoru" dir="../src/assets/endou.jpg" des="The best captain and goal keeper." />
-                <Card name="Monkey D luffy" dir="../src/assets/luffy.jpg" des="Future pirate king"/>
-                <Card name="Itachi Uchiha" dir="../src/assets/itachi.jpeg" des="Prodigy and god of genjutsu"/>
+                <Card name="Endou momoru" dir={EndouImg} des="The best captain and goal keeper." />
+                <Card name="Monkey D luffy" dir={LuffyImg} des="Future pirate king"/>
+                <Card name="Itachi Uchiha" dir={ItachiImg} des="Prodigy and god of genjutsu"/>
             </div>
         </>
     );
